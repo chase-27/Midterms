16aa6358fe0d6c4e04a87975b64c4124fc48c3df
Royce Simbillo <vincent.simbillo@gmail.com>
2025-08-18 12:18:03 +0800

    New

--- SECONDPROJECT/main.js
+++ SECONDPROJECT/main.js
@@ -1,69 +1,105 @@
-//const express = reqiure('express');
-import express from 'express'; //Same thing
-const __dirname = import.meta.dirname;
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+
+// File Path Stuff
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-//home
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'uploads/');
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage }).fields([{ name: 'file', maxCount: 1 }]);
+
+// Serve the upload form
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'admin.html'));
+});
+
+
+
+// Handle file upload post request
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) return res.status(400).send('Error uploading file');
+
+    const username = req.body.username;
+    const uploadedFile = req.files['file'][0];
+
+    console.log(`Username: ${username}`);
+    console.log(`File path: ${uploadedFile.path}`);
+
+    res.end('File and form data uploaded successfully!');
+  });
+});
+
+// Your existing routes below
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/pages/home.html');
+  res.sendFile(__dirname + '/pages/home.html');
 });
-//student form
+
 app.get('/studentForm', (req, res) => {
-    res.sendFile(__dirname + '/pages/student.html');
+  res.sendFile(__dirname + '/pages/student.html');
 });
-//admin form
+
 app.get('/adminForm', (req, res) => {
-    res.sendFile(__dirname + '/pages/admin.html');
+  res.sendFile(__dirname + '/pages/admin.html');
 });
-//profile form
+
 app.get('/profileForm', (req, res) => {
-    res.sendFile(__dirname + '/pages/profile.html');
+  res.sendFile(__dirname + '/pages/profile.html');
 });
 
-
-//API Student
 app.get('/getStudent', (req, res) => {
-    var response = {
-        studentId: req.query.studentId,
-        firstName: req.query.firstName,
-        lastName: req.query.lastName,
-        section: req.query.section
-    }
+  var response = {
+    studentId: req.query.studentId,
+    firstName: req.query.firstName,
+    lastName: req.query.lastName,
+    section: req.query.section
+  };
 
-    console.log("Response is: ", response);
-    res.end(`Recieved Data: ${JSON.stringify(response)}`);
+  console.log("Response is: ", response);
+  res.end(`Recieved Data: ${JSON.stringify(response)}`);
 });
 
-//API Admin
 app.get('/getAdmin', (req, res) => {
-    var response = {
-        adminId: req.query.adminId,
-        firstName: req.query.firstName,
-        lastName: req.query.lastName,
-        department: req.query.department
-    }
+  var response = {
+    adminId: req.query.adminId,
+    firstName: req.query.firstName,
+    lastName: req.query.lastName,
+    department: req.query.department
+  };
 
-    console.log("Response is: ", response);
-    res.end(`Recieved Data: ${JSON.stringify(response)}`);
+  console.log("Response is: ", response);
+  res.end(`Recieved Data: ${JSON.stringify(response)}`);
 });
 
-//API Profile
 app.get('/getProfile', (req, res) => {
-    var response = {
-        profileID: req.query.profileID,
-        name: req.query.name,
-        dateofbirth: req.query.dateofbirth,
-    }
+  var response = {
+    profileID: req.query.profileID,
+    name: req.query.name,
+    dateofbirth: req.query.dateofbirth,
+  };
 
-    console.log("Response is: ", response);
-    res.end(`Recieved Data: ${JSON.stringify(response)}`);
+  console.log("Response is: ", response);
+  res.end(`Recieved Data: ${JSON.stringify(response)}`);
 });
 
-const server = app.listen(5000, () =>{
-    const host = server.address().address;
-    const port = server.address().port;
-    // console.log ("Server running at http://%s:%s", host, port);
-    // console.log ("Server running at http://"+" host"+" port");
-    console.log (`Server running at http://${host}:${port}`);
-})
+const server = app.listen(5001, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`Server running at http://${host}:${port}`);
+});
